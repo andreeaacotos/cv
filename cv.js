1a0ff8e1cbb22cbf939d4b20356f72858fedb37d
andreeaacotos <78368405+andreeaacotos@users.noreply.github.com>
2021-02-16 23:33:09 +0100

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,6 @@
+const boton=document.getElementById('boton');
+boton.addEventListener('click', mostrar);
+function mostrar(){
+    document.getElementById('objective').classList.remove('hidden');
+};
+
